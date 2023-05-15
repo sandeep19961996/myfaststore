235222fa5bd8ce4058d01fe0390b5679d8cd72ce
sandeep19961996 <102844990+sandeep19961996@users.noreply.github.com>
2023-05-15 17:10:50 +0530

    vercel2

--- Shopping/settings.py
+++ Shopping/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(5#jw4284ds9uywepqvf@fldh*rs)6g3d+-c2fwgmn!kd3!mme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['faststore22.vercel.app','.now.sh','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
